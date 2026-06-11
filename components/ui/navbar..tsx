eed5e5fa3709d2642511e0f293d4152a08e5b58b
Arman Patel <vanixgaming634@gmail.com>
2026-06-12 00:10:30 +0530

    Add utility functions, Navbar component, and button component with styles

---
+++ components/ui/navbar..tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className='flex w-full justify-between items-center px-4 py-2 '>
+      <h1 className='text-[#C0C1FF]'>CodePilot AI</h1>
+    </nav>
+  )
+}
+
+export default Navbar
